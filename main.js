ca14e43cd62afb3382b82b64b4b3076d869003da
joshWyatt <joshuawyatt123@gmail.com>
2014-08-27 19:51:34 -0700

    Already used ip addresses are now stored, and cannot be voted from again

--- main.js
+++ main.js
@@ -1,28 +1,31 @@
 require('shellscript').globalize();
 
-var publicIp = $('curl icanhazip.com -s').slice(0, -1);
-console.log(publicIp);
-var offLimitIps = ['173.247.199.46', '199.87.82.66'];
+var offLimitIps = ['173.247.199.46']//, '199.87.82.66'];
 
-var canVote = true;
-for(var i = 0; i < offLimitIps.length; i++){
-  var offLimitIp = offLimitIps[i];
-  if( offLimitIp === publicIp ){
-    canVote = false;
-    break;
+
+var checkIfICanVote = function(){
+  
+  var hackerNewsLink = 'https://news.ycombinator.com/';
+
+  var publicIp = $('curl icanhazip.com -s').slice(0, -1);
+
+
+  var canVote = true;
+  for(var i = 0; i < offLimitIps.length; i++){
+    var offLimitIp = offLimitIps[i];
+    if( offLimitIp === publicIp ){
+      canVote = false;
+      break;
+    }
   }
-}
 
-if( !canVote ){
-  console.log('You cannot vote');
-}else{
-  console.log('You can vote');
-}
+  if( !canVote ){
+    console.log('You cannot vote');
+  }else{
+    offLimitIps.push(publicIp);
+    shell('open ' + hackerNewsLink);
+  }
 
+};
 
-
-
-
-
-
-// shell('open ' + hackerNewsLink);
+setInterval(checkIfICanVote, 10000);
