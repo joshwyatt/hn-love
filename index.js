1b1643d422e812693d52f9593449b3ddade58186
joshWyatt <joshuawyatt123@gmail.com>
2014-08-28 10:59:54 -0700

    Comment and clean

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('shellscript').globalize();
 
+//hard-coded HR ips. We will add ips to this as we cast our votes from them
 var offLimitIps = ['173.247.199.46', '199.87.82.66', '50.250.208.114'];
 
 
@@ -7,10 +8,12 @@ var checkIfICanVote = function(){
   
   var hackerNewsLink = 'https://news.ycombinator.com/';
 
+  //a call to this website will give us a text version of our public facing ip.
+  //it has a trailing white space we have to get rid of
   var publicIp = $('curl icanhazip.com -s').slice(0, -1);
 
+  //if we do not have wifi
   if( publicIp.length === 0 ){
-    console.log('no internet');
     return;
   }
 
@@ -24,9 +27,7 @@ var checkIfICanVote = function(){
     }
   }
 
-  if( !canVote ){
-    console.log('You cannot vote');
-  }else{
+  if( canVote ){
     offLimitIps.push(publicIp);
     shell('open ' + hackerNewsLink);
   }
