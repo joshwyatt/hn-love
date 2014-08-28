3127db1102e93698edb05e818832ccb6744883f7
joshWyatt <joshuawyatt123@gmail.com>
2014-08-28 10:47:01 -0700

    Handle situation where there is no internet

---
+++ index.js
@@ -0,0 +1,36 @@
+require('shellscript').globalize();
+
+var offLimitIps = ['173.247.199.46', '199.87.82.66', '50.250.208.114'];
+
+
+var checkIfICanVote = function(){
+  
+  var hackerNewsLink = 'https://news.ycombinator.com/';
+
+  var publicIp = $('curl icanhazip.com -s').slice(0, -1);
+
+  if( publicIp.length === 0 ){
+    console.log('no internet');
+    return;
+  }
+
+
+  var canVote = true;
+  for(var i = 0; i < offLimitIps.length; i++){
+    var offLimitIp = offLimitIps[i];
+    if( offLimitIp === publicIp ){
+      canVote = false;
+      break;
+    }
+  }
+
+  if( !canVote ){
+    console.log('You cannot vote');
+  }else{
+    offLimitIps.push(publicIp);
+    shell('open ' + hackerNewsLink);
+  }
+
+};
+
+setInterval(checkIfICanVote, 1000);
