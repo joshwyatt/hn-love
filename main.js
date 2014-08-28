826996b903da8c36c152791a2d854d36c7286b8b
joshWyatt <joshuawyatt123@gmail.com>
2014-08-27 18:53:45 -0700

    Clear trailing whitespace from ip address

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 require('shellscript').globalize();
 
+// var hrPublicFacingIp = '173.247.199.46';
+var hrPublicFacingIp = '199.87.82.66';
 var publicIp = $('curl icanhazip.com -s');
-
 var commandLineArguments = process.argv;
-
 var hackerNewsLink = commandLineArguments[2];
 
-console.log(hackerNewsLink);
+publicIp = publicIp.split('');
+publicIp.pop();
+publicIp = publicIp.join('');
+
+if( publicIp === hrPublicFacingIp ){
+  console.log('you are at hack reactor');
+}else{
+  console.log('you are not at hack reactor');
+}
+
+
+
+
+
+// shell('open ' + hackerNewsLink);
