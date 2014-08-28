7e96cd444268072e3a698090f331e86840ba9c54
joshWyatt <joshuawyatt123@gmail.com>
2014-08-28 15:59:05 -0700

    Write README. Allow passing in of optional url

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-README
+hn-love
+=======
+
+hn-love will open that HN article you wanted to vote on when you get to a location that is not HR.
+
+##Installation
+
+    npm install -g hn-love
+
+
+##Usage
+
+    hn-love [url-of-the-comments-page-for-the-article-you're-following]
+
+When you run `hn-love`, it will quietly run in the terminal. When you are connected to the internet somewhere that is not HR, Chrome will open HN so you can upvote the article you want.
+
+If you pass in the optional argument, which should be a url of the comments page for the article you would love to upvote, Chrome will open that page for you instead of the HN main page.
+
+When hn-love opens HN for you, it will add your current location to the list of places you are not allowed to vote from. As long as you keep hn-love running, it will reopen HN or the url you passed in, every time you connect to a location you haven't voted from already. Vote to your heart's content!
+
+Try using connecting your computer to your mobile hotspot to test it out.
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 'use strict';
 
-var shellscript = require('shellscript');
-shellscript.globalize();
+//shellscript will allow us to make the shell command 'curl'
+var shellscript = require('shellscript').globalize();
+
 
 //hard-coded HR ips. We will add ips to this as we cast our votes from them
 var offLimitIps = ['173.247.199.46', '199.87.82.66', '50.250.208.114'];
@@ -10,7 +11,8 @@ var offLimitIps = ['173.247.199.46', '199.87.82.66', '50.250.208.114'];
 
 var checkIfICanVote = function(){
   
-  var hackerNewsLink = 'https://news.ycombinator.com/';
+  console.log(process.argv[2]);
+  var hackerNewsLink = process.argv[2] || 'https://news.ycombinator.com/';
 
   //a call to this website will give us a text version of our public facing ip.
   //it has a trailing white space we have to get rid of
@@ -39,5 +41,5 @@ var checkIfICanVote = function(){
 
 module.exports = function(){
   console.log('hn-love is now running and will open HN for you when you are able to vote.....');
-  setInterval(checkIfICanVote, 1000);
+  setInterval(checkIfICanVote, 7000);
 }();
